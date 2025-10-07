c2de77cb6c428ab1512998bbfeb49be08f0b333a
MrPrint12 <ninamariomarioo@gmail.com>
2025-10-07 18:25:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-const USER = 'admin';
-const PASS = 'senha123';
+const USER = 'mrprint;
+const PASS = 'hack2025';
 
 app.use((req, res, next) => {
   const auth = req.headers.authorization;
